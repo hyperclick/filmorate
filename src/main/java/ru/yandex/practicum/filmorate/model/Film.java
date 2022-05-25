5bb1d97f0ce51fb1c19a8edfb04ab74741abd658
GlebInside <95642615+GlebInside@users.noreply.github.com>
2022-05-25 11:58:25 +0300

    fix desc size restriction

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -20,7 +20,7 @@ public class Film {
     @NotNull
     @NotBlank
     private String name;
-    @Max(200)
+    @Size(max = 200)
     private String description;
     private LocalDate releaseDate;
     @Min(1)
